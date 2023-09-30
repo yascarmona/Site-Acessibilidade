ef1dcf81ef09c7ebf2785d0491a83de6300bc311
yascarmona <122237495+yascarmona@users.noreply.github.com>
2023-09-30 16:07:37 -0300

    commit inicial
    O primeiro commit do site

---
+++ script.js
@@ -0,0 +1,21 @@
+Document.getElementById('meuFormulario').addEventListener('submit', function (e) {
+    var nome = document.getElementById('nome').value;
+    var fone = document.getElementById('telefone').value;
+    var email = document.getElementById('email').value;
+
+    if (!/^\d+$/.test(telefone)) {
+        alert("Por favor, insira apenas números no campo de telefone.");
+        e.preventDefault(); 
+        return;
+    }
+
+    if (email.indexOf('@') === -1) {
+        alert("Por favor, insira um endereço de email válido.");
+        e.preventDefault(); 
+        return;
+    }
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+});
